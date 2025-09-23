e3a9c74b5eb364ff32c1a0d3cbb265fb64551c0f
Aviral Mishra <aviral.mishra@gmail.com>
2025-09-23 13:28:13 +1000

    classes

--- modules-classes/test-class.js
+++ modules-classes/test-class.js
@@ -1,13 +1,41 @@
 class User {
     constructor(name, username) {
-        this.name = name;
+        this._name = name;
         this.username = username;
     }
 
+    static goodbye() {
+        console.log('Bye bye...');
+    }
+
+    get name() {
+        return this._name.toUpperCase();
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
     greet() {
-        console.log(`Hello ${this.name} (${this.username}), Have a good day!`);
+        console.log(`Hello ${this.name} (${this.username}), have a good day!`);
+    }
+}
+
+class AdminUser extends User {
+    constructor(name, username) {
+        super(name, username);
+        this.superPowers = true;
+    }
+
+    greet() {
+        console.log(`Hello admin ${this.name} (${this.username}), enjoy!`);
     }
 }
 
 let user = new User('Mark Smith', 'msmith002');
 user.greet();
+
+User.goodbye();
+
+let adminUser = new AdminUser('Adam Smith', 'asmith002');
+adminUser.greet();
